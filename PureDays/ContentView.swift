b66f4234c59ee6c0374918351e772c4c7b38a077
leon <lichenglong_0804@163.com>
2026-03-17 10:55:07 +0800

    Initial Commit

---
+++ PureDays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PureDays
+//
+//  Created by 李成龙 on 2026/3/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PureDays/PureDaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureDaysApp.swift
+//  PureDays
+//
+//  Created by 李成龙 on 2026/3/17.
+//
+
+import SwiftUI
+
+@main
+struct PureDaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
